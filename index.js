119526132a65ad7f4ec4d11d4e20caa4fee7da08
unknown <you@example.com>
2018-02-10 22:37:13 +0530

    final

---
+++ index.js
@@ -0,0 +1,32 @@
+var chalk =require("chalk"),
+  bodyParser = require("body-parser"),
+  express = require("express"),
+  cors = require("cors")
+  multer= require('multer'),
+  path = require('path');
+  var fs = require('fs');
+  var nanoid = require('nanoid')
+let app=express();
+app.use(bodyParser.json());
+app.use(cors());
+
+var  storage = multer.diskStorage({
+  destination: path.join(__dirname, '/uploads'),
+  filename: function (req, file, cb) {
+      cb(null,  nanoid(10)+path.extname(file.originalname));
+  }
+});
+var upload = multer({storage: storage});
+app.post('/fileupload',upload.single('file'),(req,res)=>{
+  var path=req.file.path;
+    res.json({path});
+
+} )
+
+
+app.set('port', process.env.PORT || 7000);
+app.set('host', process.env.HOST || '192.168.43.160');
+
+app.listen(app.get('port'),  function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      file:""
+    }
+
+  }
+    
+  async fileUpload(){
+    var element =document.getElementById('file');
+    var file=element.files[0];
+    var formData = new FormData();
+    formData.append("file",file);
+
+
+
+    await fetch('http://192.168.43.160:7000/fileupload', {
+        method: "POST",
+        headers: {
+          'Accept': 'application/json'
+          
+        },
+        body: formData  
+    
+      }).then((res)=>{
+        return res.json()
+      }).then((res)=>{
+        var file=res.path;
+this.setState({
+  file
+})          
+      })  
+  }
+  render() {
+    return (
+      <div>
+        <input type="file"  id="file" name="file"/>
+        <button onClick={ ()=>{
+          this.fileUpload()
+        }} >upload</button>
+        {this.state.file!==""?<img src={this.state.file}/>:console.log(JSON.stringify(this.state.file))}
+      </div>
+    );
+  }
+}
+
+export default App;
